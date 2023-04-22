4e4343c4a6bcce3ba9d62df25171dbb50b7f5cce
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-04-22 16:42:34 +0900

    feat: 요격시스템 lv2

---
+++ src/Algorithm/요격시스템.java
@@ -0,0 +1,34 @@
+package Algorithm;
+
+import java.util.Arrays;
+
+public class 요격시스템 {
+	public static void main(String[] args) {
+		int[][] targets = {{4,5},{4,8},{10,14},{11,13},{5,12},{3,7},{1,4}};
+		solution(targets);
+	}
+	
+	static int solution(int[][] targets) {
+        int answer = 0;
+        
+        Arrays.sort(targets, (o1, o2) -> {return o1[1]-o2[1];});
+            
+        int launch = -1;
+        
+        for(int[] target : targets){
+            if(launch==-1){
+                launch = target[1]-1;
+                answer++;
+                continue;
+            }
+            if(target[0]<=launch && target[1]>=launch){
+                continue;
+            }
+            
+            launch = target[1]-1;
+            answer++;
+        }
+        
+        return answer;
+    }
+}
