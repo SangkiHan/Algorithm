90014b521aea6958c62565466840275dd0130838
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-05-15 13:13:04 +0900

    feat: 백준 n과m3
    n과m 1과 달리 방문체크를 하지않았다.

---
+++ src/Algorithm/BackJun/BJN과M3.java
@@ -0,0 +1,35 @@
+package Algorithm.BackJun;
+
+import java.util.Scanner;
+
+public class BJN과M3 {
+	
+	static int[] arr;
+	static StringBuilder str = new StringBuilder();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		arr = new int[m];
+		
+		dfs(n, m, 0, 0);
+		
+		System.out.println(str);
+	}
+	
+	static void dfs(int n, int m, int index, int depth) {
+		if(m==depth) {
+			for(int i=0; i<m; i++) {
+				str.append(arr[i]+" ");
+			}
+			str.append("\n");
+		}
+		else {
+			for(int i=0; i<n; i++) {
+				arr[index]=i+1;
+				dfs(n, m, index+1, depth+1);
+			}
+		}
+	}
+}
